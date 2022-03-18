2e49aba6ac8a788b7a79f6c86ee9f5e04c7294c5
Eslam@DeskTop <eslam1998-dev>
2022-03-18 16:59:23 +0200

    music app

--- Music player/js/index.js
+++ Music player/js/index.js
@@ -4,20 +4,32 @@ const audio = document.querySelector(".audio");
 const totalTime = document.querySelector(".duration");
 const currentTime = document.querySelector(".currentTime");
 
+const bar = document.querySelector(".barWidth");
+const barParent = bar.parentElement;
+const totalBarWidth = parseInt(getComputedStyle(bar.parentElement).width);
+
+const classes = ["fa-caret-right", "fa-grip-lines-vertical"];
+const startCursorPoint = "3.5%";
+
 let timing;
-let min = 0, sec = 0;
+let min = 0, sec = -1, currTime = 0;
+let soundState = false;
+
+let audioDuration;
 
 run.addEventListener("click", function () {
-  const classes = ["fa-caret-right", "fa-grip-lines-vertical"];
-  if (this.classList.contains(classes[0])) {
-    playMusic(this, classes);
-    timing = setInterval(() => {
-      setDetailes();
-    }, 1000)
+  if (!soundState) {
+    checkRunBtnState(this);
   } else {
-    pausedMusic(this, classes);
-    clearInterval(timing);
+    resetSoundDetails();
+    checkRunBtnState(this)
   }
+})
+
+barParent.addEventListener("click", function (e) {
+  const pointed = e.offsetX;
+  bar.style.width = pointed + "px";
+  // console.log(Math.floor(audioDuration - totalBarWidth / pointed))
 })
 
 function playMusic(ele, cls) {
@@ -30,14 +42,19 @@ function pausedMusic(ele, cls) {
   audio.pause();
 }
 
-function setDetailes() {
-  let total;
-  audio.onloadedmetadata = function () {
-    total = (this.duration / 60).toFixed(2);
-    totalTime.innerHTML = (this.duration / 60).toFixed(2);
-  }
-  console.log(audio.duration)
+function setSoundDetailes() {
+  setAudioDuration();
+
+  const total = Math.floor(audio.duration);
   let curr = Math.floor(audio.currentTime);
+
+  if (curr === total) {
+    soundState = true;
+    run.classList.replace(classes[1], classes[0]);
+    clearInterval(timing)
+    return;
+  };
+
   if (sec < 59) {
     sec++;
     currentTime.innerHTML = min + ":" + setCurrentTime(sec);
@@ -47,9 +64,39 @@ function setDetailes() {
     currentTime.innerHTML = min + ":" + setCurrentTime(sec);
   }
   
-  function setCurrentTime(num) {
-    return num < 10 ? "0" + num : num
+  currTime = sec * totalBarWidth / total;
+  bar.style.width = currTime + "px";
+}
+setSoundDetailes()
+
+function resetSoundDetails() {
+  sec = 0, min = 0, currTime = startCursorPoint;
+  soundState = false;
+  bar.style.width = currTime;
+  currentTime.innerHTML = min + ":" + setCurrentTime(sec);
+}
+
+function setCurrentTime(num) {
+  return num < 10 ? "0" + num : num
+}
+
+function setAudioDuration() {
+  audio.onloadedmetadata = function () {
+    audioDuration = Math.floor(this.duration);
+    this.duration < 60
+    ? totalTime.innerHTML = "0:0" + Math.floor(this.duration)
+    : totalTime.innerHTML = Math.floor(this.duration)
   }
 }
-setDetailes()
 
+function checkRunBtnState(element) {
+  if (element.classList.contains(classes[0])){
+    playMusic(element, classes);
+    timing = setInterval(() => {
+      setSoundDetailes();
+    }, 1000);
+  } else {
+    pausedMusic(element, classes);
+    clearInterval(timing);
+  }
+}
